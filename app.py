01a0d3a937c75d560fab99dba52217c50d67725f
HamsiniRaparthi <raparthihamsini810@gmail.com>
2025-11-17 17:45:28 +0530

    Update app.py
    _added

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ This script runs the FlaskWebProject application using a development server.
 from os import environ
 from FlaskWebProject import app
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
