b57ba1f8abad12fc31f197664847e96e4c3932ba
HamsiniRaparthi <raparthihamsini810@gmail.com>
2025-11-18 15:51:12 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -78,6 +78,9 @@ def login():
             LOG.warning('WARNING: Login Unsucessful....Invalid username or password for user:' + str(user))
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+
+        LOG.info('INFO: User ' + str(user.username) + ' has logged in successfully.')
+
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
