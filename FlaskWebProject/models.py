41f7ef18cf810d3a596d6153306bfeb0b9b1d027
HamsiniRaparthi <raparthihamsini810@gmail.com>
2025-11-18 11:46:35 +0530

    Update models.py

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -1,4 +1,3 @@
-82% of storage used … If you run out, you can't create, edit, and upload files. Get 30 GB of storage for ₹59 ₹15/month for 3 months.
 from datetime import datetime
 from FlaskWebProject import app, db, login
 from werkzeug.security import generate_password_hash, check_password_hash
